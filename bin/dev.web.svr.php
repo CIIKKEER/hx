64acfe2388b807492b84f11039d8e2184fb62d21
BREEZZEER <lch1025@qq.com>
2026-04-20 12:29:11 +0800

    I have organized the directory layout to make the library cleaner

--- bin/dev.web.svr.php
+++ bin/dev.web.svr.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../auto.load.php';
+require_once __DIR__ . '/auto.load.php';
 
 use hx\c_base_class;
 use hx\cli\c_cli;
@@ -8,6 +8,7 @@ class c_dev_web_svr extends c_base_class
 {
 	public const version = '1.0.0';
 	public const server = 'PHP built-in web server';
+	public const release = 'Mon, Apr 20, 2026 12:25:27 PM';
 	private ?c_cli $cli = null;
 
 	public function __construct ()
@@ -21,12 +22,12 @@ class c_dev_web_svr extends c_base_class
 	}
 
 	/**
-	 * 
+	 *
 	 * @param 	int 	$argc
 	 * @param 	array 	$argv
 	 * @return 	int
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	public function main (int $argc , array $argv): int
 	{
@@ -36,8 +37,8 @@ class c_dev_web_svr extends c_base_class
 		return 0;
 	}
 
-	/* < 
-	 * 
+	/* <
+	 *
 	 */
 	private function parse_option_and_set_default_value (): self
 	{
@@ -51,40 +52,40 @@ class c_dev_web_svr extends c_base_class
 	private function on_help() : self
 	{
 		gf()->fun->cc->new()->green('usage : ')->as($this->cli->get_cmd_line_raw_path())->anl()->as('       ')
-										->cyan(' --t'			)->as(' 					/a/b/c/c/d')->as(' document root direcotory')->anl()->as('       ')
-										->cyan(' --ip'			)->as(' 					0.0.0.0')->anl()->as('       ')
-										->cyan(' --port'		)->as(' 					80')->anl()->as('       ')
-										->cyan(' --php'			)->as(' 					')->as('use the system\'s default PHP interpreter or specify a PHP version yourself')->anl()->as('       ')
-										->cyan(' --dev-web-srv-default-startup-file')->as('	')->as($this->cli->get_options()->get('dev-web-srv-default-startup-file'))->anl()->as('       ')
-										->cyan(' --help|-h'		)->as('')
-										->echo();
+		->cyan(' --t'			)->as(' 					/a/b/c/c/d')->as(' document root direcotory')->anl()->as('       ')
+		->cyan(' --ip'			)->as(' 					0.0.0.0')->anl()->as('       ')
+		->cyan(' --port'		)->as(' 					80')->anl()->as('       ')
+		->cyan(' --php'			)->as(' 					')->as('use the system\'s default PHP interpreter or specify a PHP version yourself')->anl()->as('       ')
+		->cyan(' --dev-web-srv-default-startup-file')->as('	')->as($this->cli->get_options()->get('dev-web-srv-default-startup-file'))->anl()->as('       ')
+		->cyan(' --help|-h'		)->as('')
+		->echo();
 		return $this;
 	}
 	private function parse_option (int$argc): self
 	{
 		
 		/* set the option to default value
-		 * 
+		 *
 		 */
 		$this->parse_option_and_set_default_value();
 		
 		
 		/* on help
-		 * 
+		 *
 		 */
 		if 	(1 == $argc || $this->cli->get_options()->exist('help') || $this->cli->get_options()->exist('h') || $this->cli->get_arg()->to_array()->search('help')->ok() || $this->cli->get_arg()->to_array()->search('h')->ok())
 		{
 			return $this->on_help();
 		}
 		
-				
+		
 		/* start
-		 * 
+		 *
 		 */
 		return	$this->start();
 	}
 	
-	private function start ():self 
+	private function start ():self
 	{
 		$cmd=$this->cli->get_options()->php." -S ".$this->cli->get_options()->ip.":".$this->cli->get_options()->port." -t ".$this->cli->get_options()->get('t').' '.$this->cli->get_options()->get("dev-web-srv-default-startup-file");
 		gf()->os->process->system($cmd);
@@ -99,8 +100,8 @@ function main (int $argc , array $argv): int
 }
 
 /* go ...
- * 
- * 
- * 
+ *
+ *
+ *
  */
 main($argc,$argv);
---
+++ bin/test/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../auto.load.php';
+
+gf()->fun->debug->print_r([123]);
